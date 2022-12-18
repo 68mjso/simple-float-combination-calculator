cff7301c857fa4cd8d31d6acf9d61235897268aa
miso1210 <boringsogood@gmail.com>
2022-12-18 13:53:47 +0700

    update source

--- index.py
+++ index.py
@@ -1,28 +1,35 @@
-import struct
 import numpy
 from config import *
-from ibm2ieee import *
 numpy.set_printoptions(precision=16)
+
+coreFile = open('core.txt','r')
 inputFile = open('input.txt', 'r')
 
-raw_input_arr = inputFile.read().split(',')
-arr = [numpy.float64(i) for i in raw_input_arr]
+raw_core_arr = coreFile.read().split(',')
+core_arr = [numpy.float64(i) for i in raw_core_arr]
 
+raw_input_arr = inputFile.read().split(',')
+input_arr = [numpy.float64(i) for i in raw_input_arr]
+
+arr = numpy.append(core_arr,input_arr)
 diff = max-min
 
-target_avg = numpy.float64((target + min) / diff)
-
+target_avg = numpy.float64((target - min) / diff)
 def printCombination(arr, n, r):
+    print('start')
+    print('avg: ',target_avg)
+    print('core_avg: ',sum(core_arr)/10)
+    print('diff_avg: ',(target_avg - sum(core_arr)/10)/(10 - len(core_arr))*10)
     data = [0]*r
     combinationUtil(arr, data, 0, n - 1, 0, r)
+    print('end')
 
 
 def combinationUtil(arr, data, start, end, index, r):
     if (index == r):
-        rsum = 0
-        for j in range(r):
-            rsum += data[j]
+        rsum = sum(data)
         if(numpy.float32(numpy.float64(rsum/10)) == numpy.float32(target_avg)):
+            print(data,numpy.float32(numpy.float64(rsum/10)*diff-min))
             if(numpy.float32(numpy.float64(rsum/10)*diff-min) == numpy.float32(target)):
                 print(data,numpy.float32(numpy.float64(rsum/10)*diff-min))
                 return True
@@ -30,9 +37,7 @@ def combinationUtil(arr, data, start, end, index, r):
     i = start
     while(i <= end and end - i + 1 >= r - index):
         data[index] = arr[i]
-        result = combinationUtil(arr, data, i + 1, end, index + 1, r)
-        if(result == True):
-            break
+        combinationUtil(arr, data, i + 1, end, index + 1, r)
         i += 1
 
 
